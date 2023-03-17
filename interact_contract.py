b994a4f9e3ec7dadede30eb61c4de5b406e91d52
Anonymous mouse <test@example.com>
2023-03-17 21:00:07 +0900

    Update files

--- interact_contract.py
+++ interact_contract.py
@@ -1,18 +1,13 @@
 #! /usr/bin/env python
 
-# Refere to https://coinsbench.com/how-to-deploy-and-interact-with-solidity-contracts-with-python-and-ganache-be63334323e6
-# https://k2k2-ethereum.com/programming/python/273/
-
 import sys,os
-import json
 import re
     
-from solcx import compile_standard, install_solc_pragma, compile_source
 from web3 import Web3
 
-if len(sys.argv) < 6:
-	print(f'''usage: {os.path.basename(__file__)} <ipc path> <contract address> <abi> <called function> <write/readOnly> <param(dict)>
-     ex.  {os.path.basename(__file__)} ./data_privatenet/geth.ipc
+if len(sys.argv) < 7:
+	print(f'''usage: {os.path.basename(__file__)} <ipc path> <contract address> <abi> <called function> <w/readonly> <param('a,b,c...')>
+     ex.  {os.path.basename(__file__)} ./data_privatenet/geth.ipc '0x54f...' '[{{"inputs":...' addContact w 'name,+1234...'
      
      Gethを起動しておき、そのipcのファイルを指定する
      Geth networkのアカウントリストの最初のアカウント情報を使う(eth.accounts[0]) passphraseは'a'
@@ -29,9 +24,10 @@ except Exception:
      sys.exit(1)
 called_function_name = sys.argv[4]
 read_only = sys.argv[5]
-param: dict = sys.argv[6]
+param: list = sys.argv[6].split(',')
 
 passphrase = 'a'
+
 
 def get_eoa_address_and_private_key(w3) -> tuple:
     eoa_address = w3.geth.personal.list_wallets()[0].accounts[0].address
@@ -42,6 +38,10 @@ def get_eoa_address_and_private_key(w3) -> tuple:
 
     return eoa_address, private_key
 
+# def non_tx_function():
+#     pass
+# def tx_function():
+#     pass
 
 if __name__ == '__main__':
     ######## Connect Blockchain ############
@@ -53,24 +53,21 @@ if __name__ == '__main__':
 
 
     ########## Interacting with the Contract ##############
-    print('''# ここでもtxを発行して送信するので、eth.pendingTransactions で確認すると未だ取り込まれてないtxが確認できる
-  # ここでもminer.start()が必要になる。\n''')
     #コントラクトを操作しようとするときは、コントラクトアドレスとコントラクトABI が必要
     contract = w3.eth.contract(address=contract_address, abi=abi)
-    # contact_list, tx_receipt = _addContact(contract_address, abi, '+0123456789', eoa_address, nonce)
-
     called_function = contract.functions.__getattribute__(called_function_name)
 
-
-    #read onlyの関数を呼ぶ
+    #txを発行しないread onlyの関数を呼ぶ
     if read_only == 'readonly':
-        #連絡先を取得する
-        print(f'[+] retrieve関数を呼びましたよ！\n', called_function().call())
+    #連絡先を取得する
+        print(f'[+] {called_function.fn_name}関数を呼びますよ！\n', called_function().call())
         # print(f'[+] retrieve関数を呼びましたよ！\n', contract.functions.retrieve().call())
     else:
     #txを送信する場合
+        print(f'[+] {called_function.fn_name}関数を呼びますよ！\n')
         # store_contact = contract.functions.addContact("name", '+0123456789').buildTransaction(
-        contract_tx = called_function("name", '+0123456789').buildTransaction(
+        # contract_tx = called_function(*["name", '+0123456789']).buildTransaction(
+        contract_tx = called_function(*param).buildTransaction(
             {
                 "chainId": w3.eth.chain_id, 
                 "from": eoa_address, 
@@ -82,9 +79,11 @@ if __name__ == '__main__':
         # Sign the transaction
         sign_contract_tx = w3.eth.account.sign_transaction(contract_tx, private_key=private_key)
         # Send the transaction
-        send_contract_tx = w3.eth.send_raw_transaction(sign_contract_tx.rawTransaction)
-        tx_receipt = w3.eth.wait_for_transaction_receipt(send_contract_tx)
+        send_contract_tx_hash = w3.eth.send_raw_transaction(sign_contract_tx.rawTransaction)
+        print(f'tx_hash is: {send_contract_tx_hash.hex()}')
+        print(f'Sample commands to check.. -> `eth.getTransaction("{send_contract_tx_hash.hex()}")`\n')
+        print('''# ここでもtxを発行して送信するので、eth.pendingTransactions で確認すると未だ取り込まれてないtxが確認できる
+# ここでもminer.start()が必要になる。\n''')
+        tx_receipt = w3.eth.wait_for_transaction_receipt(send_contract_tx_hash)
     
     print(f'[+] Get Block Number: {w3.eth.blockNumber}')
-
-
